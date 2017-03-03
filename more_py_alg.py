1aa890ba56e87973097259f1b56003b59159f0ac
Andy Tuttle <andytuttle12@gmail.com>
2017-03-03 13:52:24 -0500

    Checked bigO of algorhythms.

--- more_py_alg.py
+++ more_py_alg.py
@@ -1,18 +1,22 @@
 # 441 points in 20x20 square
-# def find_routes(rows,columns):
-# 	dictionary = {}
-# 	for i in range (0,rows+1):
-# 		for j in range (0,columns+1):
-# 			current_point = (i,j)
-# 			if j == 0 or i == 0:
-# 				dictionary[current_point] = 1
-# 			else:
-# 				val1 = i-1
-# 				val2 = j-1
-# 				current_val = dictionary[(val1,j)] + dictionary[(i,val2)]
-# 				dictionary[current_point] = current_val
-# 	print dictionary[rows,columns]
-# find_routes(20,20)
+import time;
+def find_routes(rows,columns):
+	dictionary = {}
+	for i in range (0,rows+1):
+		for j in range (0,columns+1):
+			current_point = (i,j)
+			if j == 0 or i == 0:
+				dictionary[current_point] = 1
+			else:
+				val1 = i-1
+				val2 = j-1
+				current_val = dictionary[(val1,j)] + dictionary[(i,val2)]
+				dictionary[current_point] = current_val
+	print dictionary[rows,columns]
+start = time.time();
+find_routes(999,999)
+elapsed = time.time() - start;
+print "<Ran in " + str(elapsed) +"s>";
 
 # def fibonacci(n):
 # 	if n < 2:
