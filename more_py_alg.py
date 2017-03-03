3f73d154ef01288da888de37c4b9175715d6d5de
Andy Tuttle <andytuttle12@gmail.com>
2017-03-03 12:58:41 -0500

    New lattice and fibonacci algorhythms.

---
+++ more_py_alg.py
@@ -0,0 +1,23 @@
+# 441 points in 20x20 square
+# def find_routes(rows,columns):
+# 	dictionary = {}
+# 	for i in range (0,rows+1):
+# 		for j in range (0,columns+1):
+# 			current_point = (i,j)
+# 			if j == 0 or i == 0:
+# 				dictionary[current_point] = 1
+# 			else:
+# 				val1 = i-1
+# 				val2 = j-1
+# 				current_val = dictionary[(val1,j)] + dictionary[(i,val2)]
+# 				dictionary[current_point] = current_val
+# 	print dictionary[rows,columns]
+# find_routes(20,20)
+
+# def fibonacci(n):
+# 	if n < 2:
+# 		return n
+# 	# if len(str((n-2) + (n-1))) == 3:
+# 	return fibonacci(n-2) + fibonacci(n-1)
+
+# print map(fibonacci, range(0, 20))
